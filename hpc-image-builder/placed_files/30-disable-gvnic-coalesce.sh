58b679beaf0164e3eac478ce08cce38d7c03dc63
abbas1902 <abbasmohamed@google.com>
2024-11-26 03:15:15 +0000

    make disable-gvnic-coalese a script and make it executable

--- hpc-image-builder/placed_files/30-disable-gvnic-coalesce.sh
+++ hpc-image-builder/placed_files/30-disable-gvnic-coalesce.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# Copyright 2024 "Google LLC"
+# Copyright 2022 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,16 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-readonly NIC="eth0"
-
-configure_interrupt_coalescing() {
-	echo "Modifying Interrupt Coalesce settings"
-	DRIVER_NAME=$(ethtool -i ${NIC} | sed -n "s/^driver:\s*//p")
-	if [[ $DRIVER_NAME == 'gve' ]]; then
-		echo "Setting values for rx-usecs and tx-usecs to 0"
-		ethtool -C ${NIC} rx-usecs 0 &>/dev/null
-		ethtool -C ${NIC} tx-usecs 0 &>/dev/null
-	else
-		echo "gVNIC driver not enabled"
-	fi
-}
+if [[ $(ethtool -i $1 | sed -n 's/^driver:\s*//p') == 'gve' ]]
+   then
+     ethtool -C $1 rx-usecs 0 &> /dev/null
+     ethtool -C $1 tx-usecs 0 &> /dev/null
+fi
